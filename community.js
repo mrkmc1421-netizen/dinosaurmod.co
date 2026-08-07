8e266c69d5b2d33819d416b6ee5ae414244fd63b
mrkmc1421-netizen <mrkmc1421@gmail.com>
2026-08-06 23:42:29 -0400

    Add community.js with T. Rex game details

---
+++ community.js
@@ -0,0 +1,28 @@
+trex dino 🦕 
+{
+  "sections": {
+    "trex": [
+      {
+        "title": "T. Rex Runner",
+        "author": "Kenny",
+        "thumbnail": "assets/trex_runner.png",
+        "tags": ["action", "dino", "endless"],
+        "projectId": "trex_runner"
+      },
+      {
+        "title": "Jurassic Beatboxer",
+        "author": "Kenny",
+        "thumbnail": "assets/jurassic_beatboxer.png",
+        "tags": ["music", "funny", "roar"],
+        "projectId": "jurassic_beatboxer"
+      },
+      {
+        "title": "T. Rex vs Volcano",
+        "author": "Kenny",
+        "thumbnail": "assets/trex_volcano.png",
+        "tags": ["adventure", "lava", "boss"],
+        "projectId": "trex_volcano"
+      }
+    ]
+  }
+        }
